53e0754319018e866c3e589171fc8f861720be21
Ian <ian.easter1@gmail.com>
2022-08-28 23:02:21 -0400

    replaced random.randomint with random.sample.  This provided a cleaner and less messy looking option to generate values between 0x00, 0xff.

--- createmac.py
+++ createmac.py
@@ -10,25 +10,27 @@ def create_random_mac(type='qemu'):
     except KeyError:
         oui = ouis['qemu']
 
-    decimal_mac = oui + [ random.randint(0x00, 0xff), random.randint(0x00, 0xff), random.randint(0x00, 0xff) ]
+    decimal_mac = oui + random.sample(range(0x00, 0xff), 3)
     mac = ':'.join(map(lambda x: "%02x" % x, decimal_mac))
     return decimal_mac, mac
 
 def main():
-    parser = argparse.ArgumentParser(description='Create random MAC with Locally Administered Organizational Unique Identifier.')
-    parser.add_argument('-c', '--count', help='Number of MACs to create.', type=int, metavar='count')
+    parser = argparse.ArgumentParser(prog='createmac',
+        description='Create random MAC with Locally Administered Organizational Unique Identifier.')
+    parser.add_argument('-c',
+                        '--count',
+                        help='Number of MACs to create.',
+                        type=int, metavar='count')
 
     args = vars(parser.parse_args())
 
-    # print(f"[debug]: Args {args}")
     if args['count'] is None:
         count = 1
     else:
         count = args['count']
 
-    # print(f"[debug]: create {count} macs")
     while count > 0:
-        decimal_mac, mac = create_random_mac()
+        _, mac = create_random_mac()
         print(mac)
         count -= 1
 
